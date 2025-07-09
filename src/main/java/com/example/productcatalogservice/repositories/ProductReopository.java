dcf76e4616f24706ae95474e37f86892ac284b1d
Kunjan <kunjan.gondalia.1@gmail.com>
2025-07-09 08:56:39 +0530

    Added custom Repository methods

--- src/main/java/com/example/productcatalogservice/repositories/ProductReopository.java
+++ src/main/java/com/example/productcatalogservice/repositories/ProductReopository.java
@@ -2,9 +2,16 @@ package com.example.productcatalogservice.repositories;
 
 import com.example.productcatalogservice.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 
 @Repository
 public interface ProductReopository extends JpaRepository<Product, Long> {
+
+//    @Query(value = "select * from products p where p.id = :id", nativeQuery = true)
+//    Product findProductWithGivenId(@Param("id") Long productId);
 }
