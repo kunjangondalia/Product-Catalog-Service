113ee875c22fcb4d6270ac4e2406255d57649690
Kunjan <kunjan.gondalia.1@gmail.com>
2025-07-09 09:18:54 +0530

    Added category Service- deleteCategory

--- src/main/java/com/example/productcatalogservice/services/CategoryService.java
+++ src/main/java/com/example/productcatalogservice/services/CategoryService.java
@@ -1,4 +1,18 @@
 package com.example.productcatalogservice.services;
 
+import com.example.productcatalogservice.models.Category;
+import com.example.productcatalogservice.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CategoryService {
+    private CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public void deleteCategory(Long categoryId) {
+        categoryRepository.deleteById(categoryId);
+    }
 }
